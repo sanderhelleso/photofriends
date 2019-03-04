bb525d805c65db61080b09f606c6ddca20f4682c
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-04 14:05:56 -0800

    added some simpel routing

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ import (
 // Send back data to matched path using the writer(res)
 func handlerFunc(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
+
+	if req.URL.Path == "/" {
+		fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
+	} else if req.URL.Path == "/contact" {
+		fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photofriends.com\">support@photofriends.com</a>.")
+	} else {
+		fmt.Fprint(res, "<h1>404! Page not found</h1>")
+	}
 }
 
 func main() {
