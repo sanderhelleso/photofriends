1802994a99bb269317aba840eae0c0511452cca4
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-04 12:57:08 -0800

    a simple web server

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+/*  Similar as node.js express (req, res)
+Send back data to matched path using the writer(w)
+*/
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
 func main() {
-	fmt.Println("Hello Word")
+	http.HandleFunc("/", handlerFunc) // route and matched content
+	http.ListenAndServe(":3000", nil) // port to serve
 }
