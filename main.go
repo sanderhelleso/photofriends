91c8f81b07f2858cefe176cbaa0c5d0fc51978a4
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-04 15:26:01 -0800

    added notfound handler and faq page

--- main.go
+++ main.go
@@ -19,9 +19,22 @@ func contact(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photofriends.com\">support@photofriends.com</a>.")
 }
 
+func faq(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(res, "Frequently asked questions: Comming Soon")
+}
+
+func notFound(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-type", "text/html")
+	res.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(res, "<p>404 page not found")
+}
+
 func main() {
-	router := mux.NewRouter()              // router
-	router.HandleFunc("/", home)           // home
-	router.HandleFunc("/contact", contact) // contact
-	http.ListenAndServe(":3000", router)   // port to serve (nil = NULLPOINTER)
+	router := mux.NewRouter()                           // router
+	router.NotFoundHandler = http.HandlerFunc(notFound) // 404 not found
+	router.HandleFunc("/", home)                        // home
+	router.HandleFunc("/contact", contact)              // contact
+	router.HandleFunc("/fag", faq)                      // faq
+	http.ListenAndServe(":3000", router)                // port to serve (nil = NULLPOINTER)
 }
