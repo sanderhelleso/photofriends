75b614637a72b9b28b8cb7b818b2b819a6e8a753
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-04 15:13:27 -0800

    implemented own functions to handle individual routes

--- main.go
+++ main.go
@@ -9,23 +9,19 @@ import (
 
 // Similar as node.js express (req, res)
 // Send back data to matched path using the writer(res)
-func handlerFunc(res http.ResponseWriter, req *http.Request) {
-
-	// set content type to html
+func home(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
+}
 
-	if req.URL.Path == "/" { // home
-		fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
-	} else if req.URL.Path == "/contact" { // contact
-		fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photofriends.com\">support@photofriends.com</a>.")
-	} else { // 404
-		res.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(res, "<h1>404! Page not found</h1>")
-	}
+func contact(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photofriends.com\">support@photofriends.com</a>.")
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", r) // port to serve (nil = NULLPOINTER)
+	router := mux.NewRouter()              // router
+	router.HandleFunc("/", home)           // home
+	router.HandleFunc("/contact", contact) // contact
+	http.ListenAndServe(":3000", router)   // port to serve (nil = NULLPOINTER)
 }
