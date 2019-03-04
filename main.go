ed5f2d7819280bc9d85dd81c153d573c1c6be4a3
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-04 13:53:50 -0800

    added content type

--- main.go
+++ main.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
-/*  Similar as node.js express (req, res)
-Send back data to matched path using the writer(w)
-*/
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+// Similar as node.js express (req, res)
+// Send back data to matched path using the writer(res)
+func handlerFunc(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc) // route and matched content
-	http.ListenAndServe(":3000", nil) // port to serve
+	http.ListenAndServe(":3000", nil) // port to serve (nil = NULLPOINTER)
 }
