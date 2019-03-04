0c38ba76a27ad2447625bd50e15ab514b23fa618
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-04 14:12:57 -0800

    added 404 status code

--- main.go
+++ main.go
@@ -8,13 +8,16 @@ import (
 // Similar as node.js express (req, res)
 // Send back data to matched path using the writer(res)
 func handlerFunc(res http.ResponseWriter, req *http.Request) {
+
+	// set content type to html
 	res.Header().Set("Content-Type", "text/html")
 
-	if req.URL.Path == "/" {
+	if req.URL.Path == "/" { // home
 		fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
-	} else if req.URL.Path == "/contact" {
+	} else if req.URL.Path == "/contact" { // contact
 		fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photofriends.com\">support@photofriends.com</a>.")
-	} else {
+	} else { // 404
+		res.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(res, "<h1>404! Page not found</h1>")
 	}
 }
