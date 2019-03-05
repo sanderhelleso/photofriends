bd618206cd23354bf5862cfdf33f1e0d2bcf1fac
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-05 00:59:51 -0800

    more sql test, can now write data to postgres DB

--- exp/main.go
+++ exp/main.go
@@ -20,15 +20,25 @@ func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 	host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil { panic(err) }
+	defer db.Close()
+
+	var id int
+	err = db.QueryRow(`
+		INSERT INTO users(name, email)
+		VALUES ($1, $2)
+		RETURNING id`, 
+		"John Doe", "johndoe@gmail.com").Scan(&id)
+
 	if err != nil {
 		panic(err)
 	}
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected!")
-	db.Close()
+	fmt.Println("new id is...", id)
 }
+
+/*CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	email TEXT NOT NULL
+);*/
