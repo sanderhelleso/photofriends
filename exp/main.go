3833fc4cde63a0e8824d8d43e85d9f2dd74bec36
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-05 14:07:09 -0800

    can now create user

--- exp/main.go
+++ exp/main.go
@@ -23,8 +23,19 @@ func main() {
 	}
 
 	defer us.Close()
-	us.DestructiveReset()
+	/*us.DestructiveReset()
 
-	//user, err := us.ByID(1)
-	//fmt.Println(user)
+	user := models.User {
+		Name: "Michael Scott",
+		Email: "michael@dundermifflin.com",
+	}
+
+	if err := us.Create(&user); err != nil {
+		panic(err)
+	}*/
+
+	user, err := us.ByID(1)
+	if err != nil { panic(err) }
+
+	fmt.Println(user)
 }
