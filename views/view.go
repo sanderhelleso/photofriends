394dc39797c361c2c2f8f30c518765f9438fdff2
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-04 16:29:28 -0800

    Create view.go

---
+++ views/view.go
@@ -0,0 +1,24 @@
+package views
+
+import "html/template"
+
+// note the "...string" syntax, it means that the function
+// can take in "n" amount of arguments of the type string
+func NewView(files ...string) *View {
+	files = append(files, "views/layouts/footer.gohtml")
+
+	t, err := template.ParseFiles(files...) // spread strings from arr
+	if err != nil {
+		panic(nil)
+	}
+
+	return &View{
+		Template: t,
+	}
+}
+
+// View structure to initialize "n" amount
+// of passed inn template paths
+type View struct {
+	Template *template.Template
+}
