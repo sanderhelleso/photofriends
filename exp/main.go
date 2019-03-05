7fc6aa9020a2dfbcd147027eb82c35cfc60f3ea3
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-05 13:30:39 -0800

    updated users model with helper funcs

--- exp/main.go
+++ exp/main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"fmt"
-	"bufio"
-	"os"
-	"strings"
-
-	"github.com/jinzhu/gorm"
-  	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"../../photofriends/models"
 )
 
 const (
@@ -19,58 +14,17 @@ const (
 	dbname 	 = "photofriends_dev"
 )
 
-type User struct {
-	gorm.Model // id, created at, deleted at, updated at
-	Name string
-	Email string
-}
-
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 	host, port, user, password, dbname)
 
-	db, err := gorm.Open("postgres", psqlInfo)
-	
-	if err != nil { panic(err) }
-
-	defer db.Close()
-	if err := db.DB().Ping(); err != nil {
-		panic(err)
+	us, err := models.NewUserService(psqlInfo); if err != nil {
+		panic(err);
 	}
 
-	//db.DropTableIfExists($User{})
-	db.LogMode(true)
-	db.AutoMigrate(&User{}) // automigrates the user struct as table
+	defer us.Close()
+	us.DestructiveReset()
 
-	name, email := getInfo()
-	user := User {
-		Name: name,
-		Email: email,
-	}
-
-	if err := db.Create(&user).Error; err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("%+v\n", user)
+	//user, err := us.ByID(1)
+	//fmt.Println(user)
 }
-
-func getInfo() (name, email string) {
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Println("What is your name?")
-	name, _ = reader.ReadString('\n')
-	fmt.Println("What is your email?")
-	email, _ = reader.ReadString('\n')
-
-	name = strings.TrimSpace(name)
-	email = strings.TrimSpace(email)
-
-	return;
-}
-
-/*CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	email TEXT NOT NULL
-);*/
--- models/users.go
+++ models/users.go
@@ -1,9 +1,60 @@
 package models
 
 import (
+	"errors"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var (
+	// ErrNotFound is returned when a resource
+	// cannot be found in the database
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {	
+		return nil, err
+	}
+
+	return &UserService {
+		db: db,
+	}, nil
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+// ByID will look up a user by the id provided
+// Cases:
+// 1 - user, nil 		- User found
+// 2 - nil, ErrNotFound	- User not found
+// 3 - nil, otherError  - Database error
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}	
+}
+
+// closes the UserService database connection
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+// drops the user table and rebuilds it
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
 
 type User struct {
 	gorm.Model
