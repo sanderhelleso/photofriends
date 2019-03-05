a23e99d59aeb4027e6b878c0b8dbcf6b1f14efd1
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-04 18:50:13 -0800

    added signup page

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
+	signupView 	*views.View
 )
 
 // Similar as node.js express (req, res)
@@ -25,6 +26,11 @@ func home(res http.ResponseWriter, req *http.Request) {
 func contact(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "text/html")
 	must(contactView.Render(res, nil))
+}
+
+func signup(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(res, nil))
 }
 
 func faq(res http.ResponseWriter, req *http.Request) {
@@ -43,12 +49,14 @@ func main() {
 	// view setup
 	homeView = views.NewView("layout", "views/home.gohtml")
 	contactView = views.NewView("layout", "views/contact.gohtml")
+	signupView = views.NewView("layout", "views/signup.gohtml")
 
 	// router & path config
 	router := mux.NewRouter()                           // router
 	router.NotFoundHandler = http.HandlerFunc(notFound) // 404 not found
 	router.HandleFunc("/", home)                        // home
-	router.HandleFunc("/contact", contact)              // contact
+	router.HandleFunc("/contact", contact) 
+	router.HandleFunc("/signup", signup)             // contact
 	router.HandleFunc("/fag", faq)                      // faq
 	http.ListenAndServe(":3000", router)                // port to serve (nil = NULLPOINTER)
 }
