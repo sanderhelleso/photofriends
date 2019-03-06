1bdf981e796f6bd6820d2b442862d0a9e141a652
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-06 06:44:25 -0800

    added bcrypt for password hashing

--- models/users.go
+++ models/users.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"errors"
+	"golang.org/x/crypto/bcrypt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -73,6 +74,11 @@ func first(db *gorm.DB, dst interface{}) error {
 // will create the provided user and backfill data
 // like the ID, CreatedAt and UpdatedAt fields
 func (us *UserService) Create(user *User) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil { return err }
+
+	user.PasswordHash = string(hashedBytes) // convert byteslice to string
+	user.Password = ""
 	return us.db.Create(user).Error
 }
 
@@ -114,6 +120,8 @@ func (us *UserService) AutoMigrate() error {
 
 type User struct {
 	gorm.Model
-	Name string
-	Email string `gorm:"not null;unique_index"`
+	Name 		 string
+	Email	  	 string `gorm:"not null;unique_index"`
+	Password 	 string `gorm:"-"` // ignore in DB
+	PasswordHash string `gorm:"not null"`
 }
