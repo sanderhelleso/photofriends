1627405aaa34605b760c23b181583b122ccb1700
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-19 19:44:21 -0700

    Implemented the title and userID required validators

--- models/galleries.go
+++ models/galleries.go
@@ -1,6 +1,16 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	ErrUserIDRequired = errors.New("User ID is required")
+
+	ErrTitleRequired = errors.New("Title is required")
+)
 
 // Gallery is our image container resource
 // that visitors will view
@@ -32,6 +42,34 @@ type galleryValidator struct {
 	GalleryDB
 }
 
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFuncs(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+
+	if err != nil {
+		return err
+	}
+
+	return gv.GalleryDB.Create(gallery)
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+
+	return nil
+}
+
 // ensure interface is valid
 var _ GalleryDB = &galleryGorm{}
 
@@ -41,4 +79,16 @@ type galleryGorm struct {
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
+}
+
+type galleryValFunc func(*Gallery) error
+
+func runGalleryValFuncs(gallery *Gallery, fns ...galleryValFunc) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
